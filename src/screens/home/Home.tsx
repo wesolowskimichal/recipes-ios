f37c2a5a5b40f0a959f937516904abbb7f2a079c
Michal Wesolowski <michal.wesolov@gmail.com>
2024-07-10 21:21:31 +0200

    fetching data from fake api in home screen and fixed status bar offset

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,40 @@
 import { useEffect, useState } from 'react'
 import { HomeScreenProps, Recipe } from '../../types/Types'
 import axios from 'axios'
-import { Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, StatusBar as StatusBarRN } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 
 const Home = ({ navigation, route }: HomeScreenProps) => {
-  return <Text>Home Screen</Text>
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await axios.get('https://dummyjson.com/recipes')
+      if (response.status !== 200) {
+        console.log('error while fetching from api')
+        return
+      }
+      setRecipes(response.data)
+    }
+
+    fetchRecipes()
+  }, [])
+
+  return (
+    <>
+      <StatusBar style="auto" translucent />
+      <SafeAreaView style={styles.root}>
+        <Text>hello</Text>
+      </SafeAreaView>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    marginTop: StatusBarRN.currentHeight
+  }
+})
 
 export default Home
